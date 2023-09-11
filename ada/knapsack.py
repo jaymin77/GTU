9f388374c223f38078e208fde3b534bff419e362
RaviPatel04 <ravidpatel2082004@gmail.com>
2023-09-11 14:36:36 +0530

    ada
    knapsack

---
+++ ada/knapsack.py
@@ -0,0 +1,28 @@
+def knapsack(profit,weight,capacity):
+    
+    ratio = [p/w for p,w in zip(profit,weight)]
+    print("ratio:",ratio)
+    n = len(weight)
+    new = list(range(n))
+    new.sort(key= lambda i: ratio[i],reverse=True)
+    print("new:",new)
+    max_value = 0
+    fractions = [0]*n
+    for i in new:
+        if weight[i] <= capacity:
+            max_value += profit[i]
+            capacity -= weight[i]
+            fractions[i] = 1
+        else:
+            fractions[i] = capacity / weight[i]
+            max_value += profit[i]*fractions[i]  
+            break
+    print("fractions:",fractions)
+    return max_value
+
+
+
+a = knapsack([25,24,15],[18,15,10],20)
+# a = knapsack([5,10,15,7,8,9,4],[1,3,5,4,1,3,2],15)
+
+print(a)
