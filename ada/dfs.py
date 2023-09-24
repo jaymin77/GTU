5650f2980c28778cd1a9722637ddc009e534b0ba
RaviPatel04 <ravidpatel2082004@gmail.com>
2023-09-24 16:17:19 +0530

    dfs ada

--- ada/dfs.py
+++ ada/dfs.py
@@ -1,12 +1,12 @@
 def dfs(graph, node, visited):
     if node not in visited:
         print(node, end=' ')
-        visited.add(node)
+        visited.append(node)
         for i in graph[node]:
             dfs(graph, i, visited)
 
 
 # graph = {0:[1,3], 1:[2,3,5,6], 2:[1,3,4,5], 3:[1,2,4], 4:[2,3,6], 5:[1,2], 6:[1,4]}
 graph = {0:[1,3], 1:[3,5,6], 2:[4,5], 3:[1,4], 4:[2,3,6], 5:[1,2], 6:[1,4]}
-visited_nodes = set()
+visited_nodes = []
 dfs(graph, 0, visited_nodes)
