2d10119cb05a9d374be4e8d2f22e561bfd4796d1
RaviPatel04 <ravidpatel2082004@gmail.com>
2023-10-24 09:26:43 +0530

    PDS

--- ada/5_knapsackdyna.py
+++ ada/5_knapsackdyna.py
@@ -1,16 +1,43 @@
-def knapsack(profit,weight,capacity,n):
-    if n == 0 or capacity == 0:
-        return 0
-    if weight[n-1]>capacity:
-        return knapsack(profit, weight, capacity,n-1)
-    else:
-        return max(profit[n-1]+knapsack(profit,weight,capacity-weight[n-1],n-1),knapsack(profit,weight,capacity,n-1))
-                
+def knapsackdyna(profit, weight, W):
+    n = len(profit)
 
-capacity = 4
-profit = [1,2,3]
-weight = [4,5,1]
-n = len(weight)
-print(knapsack(profit, weight, capacity, n))
-                
-        
+    V = [[0 for _ in range(W + 1)] for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for j in range(W + 1):
+            if j < weight[i - 1]:
+                V[i][j] = V[i - 1][j]
+            else:
+                V[i][j] = max(V[i - 1][j], V[i - 1][j - weight[i - 1]] + profit[i - 1])
+
+    max_profit = V[n][W]
+
+    return V, max_profit
+
+Profit = [1, 6, 18, 22, 28]
+weight = [1, 2, 5, 6, 7]
+W = 11
+
+V, max_profit = knapsackdyna(Profit, weight, W)
+
+for row in V:
+    print(row)
+
+print("Maximum Value:", max_profit)
+
+
+# Find the selected items
+#     selected_items = []
+#     i, j = n, W
+#     while i > 0 and j > 0:
+#         if V[i][j] != V[i - 1][j]:
+#             selected_items.append(i - 1)
+#             j -= weights[i - 1]
+#         i -= 1
+    
+#     selected_items.reverse()
+
+#     for row in V:
+#         print(row)
+    
+#     return max_value, selected_items
