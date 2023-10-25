9c812284f8206bc5bd4ae34c0f88a69892d7cc21
RaviPatel04 <ravidpatel2082004@gmail.com>
2023-10-25 22:41:41 +0530

    PDS

---
+++ pds/1_calculator.py
@@ -0,0 +1,9 @@
+ele1 = int(input("Enter 1st num: "))
+ele2 = int(input("Enter 2nd num: "))
+
+print(f"addition: {ele1 + ele2}")
+print(f"subtraction: {ele1 - ele2}")
+print(f"multiplicaton: {ele1 * ele2}")
+print(f"division: {ele1 / ele2}")
+print(f"floor division: {ele1 // ele2}")
+print(f"exponent: {ele1 ** ele2}")
